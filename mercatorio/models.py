b7e4fcbf811d6645f4d7a4d0ea6fe108dba2bbda
claytongit <claytontavares61@gmail.com>
2025-05-19 20:32:19 -0300

    Create Precatorio Model

--- mercatorio/models.py
+++ mercatorio/models.py
@@ -9,3 +9,14 @@ class Creditor(models.Model):
 
     def __str__(self):
         return f"{self.name} ({self.cpf_cnpj})"
+
+class Precatorio(models.Model):
+    precatorio_number = models.CharField(max_length=100, unique=True)
+    nominal_value = models.DecimalField(max_digits=15, decimal_places=2)
+    forum = models.CharField(max_length=255)
+    publication_date = models.DateField()
+
+    creditor = models.ForeignKey(Creditor, on_delete=models.CASCADE, related_name='precatorios')
+
+    def __str__(self):
+        return f"Precatório {self.precatorio_number} - {self.creditor.name}"
--- mercatorio/tests.py
+++ mercatorio/tests.py
@@ -1,7 +1,7 @@
 from django.test import TestCase
 from django.db import IntegrityError
 
-from .models import Creditor
+from .models import Creditor, Precatorio
 
 # Create your tests here.
 class ModelsTest(TestCase):
@@ -11,6 +11,13 @@ class ModelsTest(TestCase):
             cpf_cnpj="123.456.789-00",
             email="test@example.com",
             phone="(11) 9999-9999"
+        )
+        self.precatorio = Precatorio.objects.create(
+            precatorio_number = "0001234-56.2020.8.26.0050",
+            nominal_value = 50000.00,
+            forum = "TJSP",
+            publication_date = "2023-10-01",
+            creditor = self.creditor
         )
 
     def test_creditor_creation(self):
@@ -28,3 +35,33 @@ class ModelsTest(TestCase):
                 email="duplicate@example.com",
                 phone="(11) 8888-8888"
             )
+
+    def test_precatorio_creation(self):
+        self.assertEqual(self.precatorio.precatorio_number, "0001234-56.2020.8.26.0050")
+        self.assertEqual(self.precatorio.nominal_value, 50000.00)
+        self.assertEqual(self.precatorio.forum, "TJSP")
+        self.assertEqual(self.precatorio.publication_date, "2023-10-01")
+        self.assertEqual(self.precatorio.creditor, self.creditor)
+        self.assertEqual(str(self.precatorio), "Precatório 0001234-56.2020.8.26.0050 - Test Creditor")
+    
+    def test_precatorio_creation_invalid_unique_precatorio_number(self):
+        with self.assertRaises(IntegrityError):
+            Precatorio.objects.create(
+                precatorio_number = "0001234-56.2020.8.26.0050",  # Same precatorio_number
+                nominal_value = 60000.00,
+                forum = "TJSP",
+                publication_date = "2023-10-02",
+                creditor = self.creditor
+            )
+    
+    def test_precatorio_creation_invalid_creditor_null(self):
+        with self.assertRaises(IntegrityError):
+            Precatorio.objects.create(
+                precatorio_number = "0001234-56.2020.8.26.0051",
+                nominal_value = 60000.00,
+                forum = "TJSP",
+                publication_date = "2023-10-02",
+                creditor = None # Creditor Null
+            )
+
+    
