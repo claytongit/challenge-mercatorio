1c33aacd36fecfed3127b30e3cdd387376bb4f67
claytongit <claytontavares61@gmail.com>
2025-05-20 01:36:39 -0300

    setting admin dash

--- mercatorio/admin.py
+++ mercatorio/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
+from .models import Creditor, Precatorio
+
 # Register your models here.
+@admin.register(Creditor)
+class CreditorAdmin(admin.ModelAdmin):
+    list_display = ('id','name',)
+    ordering = ('-id',)
+
+class PrecatorioAdmin(admin.ModelAdmin):
+    list_display = ('id','precatorio_number','creditor')
+    ordering = ('-id',)
+
+admin.site.register(Precatorio, PrecatorioAdmin)
